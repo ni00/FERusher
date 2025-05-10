2e1b5e3e816fd20688b6dd510c4d185c911ad9d1
ni00 <mail@nixiaobai.com>
2025-05-10 22:17:22 +0800

    feat: support vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import './globals.css'
 import { ThemeProvider } from '@/components/theme-provider'
+import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata: Metadata = {
   title: 'FERusher | 切图仔，冲冲冲！',
@@ -23,6 +25,8 @@ export default function RootLayout({
         >
           {children}
         </ThemeProvider>
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
