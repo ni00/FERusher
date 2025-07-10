0e3bfe7ba1b3649c02c68537090ccd61bd6f9391
Otakus0320 <jioboyang@gmail.com>
2025-07-10 11:44:18 +0800

    chore(config): setup pnpm commit with cz and commitlint

---
+++ .commitlintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+    extends: ['cz'],
+
+    rules: {
+        // 作用范围
+        'scope-enum': [2, 'always', ['app', 'ui', 'deps', 'config', 'workflow']],
+        // 主题大小写
+        'subject-case': [
+            2,
+            'never',
+            ['sentence-case', 'start-case', 'pascal-case', 'upper-case'],
+        ],
+        // 主题不能为空
+        'subject-empty': [2, 'never'],
+        // 主题长度
+        'subject-max-length': [2, 'always', 50],
+        // 类型不能为空
+        'type-empty': [2, 'never'],
+        // Header 最大长度
+        'header-max-length': [2, 'always', 72],
+        // Body 行最大长度
+        'body-max-line-length': [2, 'always', 100],
+        // Footer 行最大长度
+        'footer-max-line-length': [2, 'always', 100],
+    },
+    useEmoji: true,
+};
